70077b48e8ed8262a2c0ec3d40a122bede49b22d
J0hn-C77n <154112993+J0hn-C77n@users.noreply.github.com>
2023-12-22 04:55:38 +0300

    Update multiple_ping.py
    some changes to not use uninvented modules right now

--- multiple_ping.py
+++ multiple_ping.py
@@ -1,20 +1,30 @@
+#python 3.11.7
+
 import os
 
 hosts = input('specify path to file \n$ ')
 print ('\n')
-quiet_mode = input('would you like to recive only result of scan? \ny|n \n$ ')
-print ('\n')
-is_up_or_known = input('Would you ike to know is host is UP, DOWN and ')
+
+
+        # add quiet mode
+        # add filtering to Host Up | Host DOWN | Host is Not Known
+
+
+
+
+
+
+#quiet_mode = input('would you like to receive only result of scan? \nyes / no \n$ ')
+#print ('\n')
+#is_up_or_known = input('Would you like to know is host is UP, DOWN and ')
 
 def pure_scan(hosts):
 
     for DNS_name in open(hosts,'r'):
         response = os.system("ping -c 1 " + DNS_name)
-    print()
+    print()  # This print is for testing purposes only, but maybe it's going to stay here
 
 
-        # add quiet mode
-        # add filtering to Host Up | Host DOWN | Host is not Known
         
 #def quiet(hosts):
         
